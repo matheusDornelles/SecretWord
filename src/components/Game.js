f1eff9c1d099f4349c1bc8ae9c7a7c2e3a31231a
Matheus Dornelles <mdmaia91@gmail.com>
2022-11-15 01:07:02 -0300

    att telas e css

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,9 +2,29 @@ import "./Game.css";
 
 const game = ({verifyLetter}) => {
   return (
-    <div>
-      <h2>verifyLetter</h2>
-      <button onClick={verifyLetter}>Começar</button>
+    <div className="game">
+      <p className="points">
+        <span>Pontuação: 000</span>
+      </p>
+      <h1>Adivinhe a palavra:</h1>
+      <h3 className="tip">
+          Dica sobre a palavra: <span>Dica...</span>
+      </h3>
+      <div className="wordContainer">
+        <span className="letter">A</span>
+        <span className="blankSquare"></span>
+      </div>
+      <div className="letterContainer">
+        <p>Tente adivinhar a letra da palavra</p>
+        <form>
+          <input type="text" name="letter" maxLength="1" required />
+          <button>Jogar</button>
+        </form>
+      </div>
+      <div className="wrongLetterContainer"></div>
+      <p>Letras já utilizadas</p>
+      <span>a, </span>
+      <span>b, </span>
     </div>
   )
 }
