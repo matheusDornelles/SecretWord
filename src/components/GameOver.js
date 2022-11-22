093331d087b6dc5f3ddd7030b9ea2e28fc8de3e5
Matheus Dornelles <mdmaia91@gmail.com>
2022-11-22 17:17:08 -0300

    valid and invalid letters, and game over

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -3,7 +3,7 @@ import "./GameOver.css"
 const gameover = ({retry}) => {
   return (
     <div>
-      <h1>Quero seu cuzinho</h1>
+      <h1>Você Perdeu</h1>
         <button onClick={retry}>Resetar Jogo</button>
     </div>
   )
