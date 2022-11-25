9dd91de01db6048b1758d978268b9caf4f230389
Matheus Dornelles <mdmaia91@gmail.com>
2022-11-24 21:30:40 -0300

    finishing the game

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -1,12 +1,15 @@
-import "./GameOver.css"
+import "./GameOver.css";
 
-const gameover = ({retry}) => {
+const GameOver = ({ retry, score }) => {
   return (
-    <div>
-      <h1>Você Perdeu</h1>
-        <button onClick={retry}>Resetar Jogo</button>
+    <div className="gameover">
+      <h1>Fim de jogo!</h1>
+      <h2>
+        A sua pontuação foi: <span>{score}</span>!
+      </h2>
+      <button onClick={retry}>Reiniciar</button>
     </div>
-  )
-}
+  );
+};
 
-export default gameover
+export default GameOver;
